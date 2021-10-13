afd9d431a7579a2d4a571b3a547043aa5a98c4c0
shubham kushwaha <shubhkush1234@gmail.com>
2021-10-13 13:34:58 +0530

    added basic accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React , {useState} from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+
+
+  const [activeIndex, setActiveIndex] = useState(null)
+
+  const onTitleClick = (index) => {
+    console.log("clicked", index);
+    setActiveIndex(index);
+  }
+
+
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className="title active"
+          onClick={() => onTitleClick(index)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
@@ -15,7 +27,9 @@ const Accordion = ({ items }) => {
     );
   });
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return <div className="ui styled accordion">
+            {renderedItems} <h1>{activeIndex}</h1>
+        </div>;
 };
 
 export default Accordion;
